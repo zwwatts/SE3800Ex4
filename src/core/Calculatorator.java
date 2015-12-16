d7bccb8c7b214edcfd51dce4e1db2e06348fe2d6
Gerald Soriano <sorianog@msoe.edu>
2015-12-16 15:52:01 -0600

    Added method stubs for the Calculatorator class

--- src/core/Calculatorator.java
+++ src/core/Calculatorator.java
@@ -1,5 +1,46 @@
 package core;
 
-public class Calculatorator {
+/**
+ * SE3800-002
+ * Exercise 3
+ * @author sorianog, wattsz
+ *
+ * This class represents a Calculator object specifying certain mathematical/calculator related methods.
+ */
 
+public class Calculatorator {
+	
+	public int add(){
+		 
+		return 0;
+	}
+	
+	public int sub() {
+		
+		return 0;
+	}
+	
+	public int mul() {
+		
+		return 0;
+	}
+	
+	public int div() {
+		
+		return 0;
+	}
+	
+	// TODO: parking the implmentation of this function 
+	// We discussed possibly having a History object 
+	public String hist() {
+		return "";
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public void pastRes() {
+		
+	}
 }
