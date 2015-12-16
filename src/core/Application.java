a936e141905d3c50369433aa4591bcd736178deb
wattsz <wattsz@msoe.edu>
2015-12-16 15:51:50 -0600

    added the general running loop

--- src/core/Application.java
+++ src/core/Application.java
@@ -1,13 +1,29 @@
 package core;
 
+import java.util.*;
 public class Application {
 	
 	public static void main(String[] args){
 		boolean running = true;
+		Calculatorator calc = new Calculatorator();
+		Scanner stdIn = new Scanner(System.in);
 		while(running){
-			
-			
+			String command = stdIn.next();
+			String[] parsedCommand;
+			try {
+				parsedCommand = command.split(" ");
+				
+				switch (parsedCommand[0]) {
+				case "add":
+					calc.add();
+					break;
+
+				default:
+					break;
+				}
+			} catch (Exception e) {
+				// TODO: Elegantly handle this ==============================================================================================
+			}
 		}
 	}
-
 }
